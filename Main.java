d1c19d92a6e9076613107d272515702188c34a90
rpate120 <rpate120@uncc.edu>
2017-09-19 15:26:35 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,111 @@
+package com.company;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    private static class Board{
+        Random random = new Random();
+
+        int[] rows;
+
+        Board (int n){
+            rows = new int[n];
+            scramble();
+        }
+
+        void scramble(){
+            for (int i = 0, n = rows.length; i < n; i++){
+                rows[i] = i;
+            }
+            for (int i = 0, n = rows.length; i < n; i++){
+                int j = random.nextInt(n);
+                int rowToSwap = rows[i];
+                rows[i] = rows[j];
+                rows[j] = rowToSwap;
+            }
+        }
+
+        int conflicts(int row, int col){
+            int count = 0;
+            for (int c = 0; c < rows.length; c++){
+                if (c == col) continue;
+                int r = rows[c];
+                if (r == row || Math.abs(r-row) == Math.abs(c-col))
+                    count++;
+            }
+            return count;
+        }
+
+        void solve(){
+            int moves = 0;
+
+            ArrayList<Integer> candidates = new ArrayList<Integer>();
+
+            while (true){
+                int maxConflicts = 0;
+                candidates.clear();
+                for (int c = 0; c < rows.length; c++){
+                    int conflicts = conflicts(rows[c], c);
+                    if (conflicts == maxConflicts)
+                        candidates.add(c);
+                    else if (conflicts > maxConflicts){
+                        maxConflicts = conflicts;
+                        candidates.clear();
+                        candidates.add(c);
+                    }
+                }
+
+                if (maxConflicts == 0)
+                    return;
+
+                int worstQueenColumn = candidates.get(random.nextInt(candidates.size()));
+
+                int minConflicts = rows.length;
+                candidates.clear();
+                for (int r = 0; r < rows.length; r++){
+                    int conflicts = conflicts(r, worstQueenColumn);
+                    if (conflicts == minConflicts)
+                        candidates.add(r);
+                    else if (conflicts < minConflicts){
+                        minConflicts = conflicts;
+                        candidates.clear();
+                        candidates.add(r);
+                    }
+                }
+
+                if (!candidates.isEmpty())
+                    rows[worstQueenColumn] = candidates.get(random.nextInt(candidates.size()));
+
+                moves++;
+                if (moves == rows.length*2){
+                    scramble();
+                    moves = 0;
+                }
+
+
+            }
+        }
+
+        void print(PrintStream stream){
+            for (int r = 0; r < rows.length; r++){
+                for (int c = 0; c < rows.length; c++){
+                    stream.print(rows[c] == r ? '1' : '0');
+                }
+                stream.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Board board = new Board(8);
+        long start = System.currentTimeMillis();
+        board.solve();
+        long stop = System.currentTimeMillis();
+        System.out.println("Found in " + ((double)(stop-start))/1000 + "s.");
+        board.print(System.out);
+    }
+}
